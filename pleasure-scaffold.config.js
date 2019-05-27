d23b161a32fce3a98ee9b1ad3c1b291bff821c5d
Martin Rafael Gonzalez <tin@devtin.io>
2019-05-27 15:03:43 -0400

    initial

---
+++ pleasure-scaffold.config.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+  install ({ options, fileMap }) {
+    return {
+      options,
+      fileMap
+    }
+  },
+  // see: https://github.com/terkelg/prompts
+  prompts: {
+
+  }
+}
